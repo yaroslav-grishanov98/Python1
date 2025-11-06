c3ba266fde02dc268e8109f63bfcfe8b05a0fd56
Yaroslav <yaroslav.grishanov.98@bk.ru>
2025-11-06 15:00:14 +0300

    dz

--- generics/settings.py
+++ generics/settings.py
@@ -152,3 +152,11 @@ CELERY_ACCEPT_CONTENT = ['json']
 CELERY_RESULT_BACKEND = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'UTC'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
